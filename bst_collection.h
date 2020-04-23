d8cb164d991cc0f683fc228c004fdcc69ee12193
CoderKevL <Kevin.Lunden@gmail.com>
2020-04-23 15:14:00 -0700

    Almost done

--- bst_collection.h
+++ bst_collection.h
@@ -1,10 +1,9 @@
 
 //----------------------------------------------------------------------
-// Author: ...
+// Author: Kevin Lunden
 // Course: CPSC 223, Spring 2020
 // Assign: 9
 // File:   bst_collection.h
-// ...
 //----------------------------------------------------------------------
 
 
@@ -83,7 +82,7 @@ private:
 
   // recursively (deep) copy ancestors of src to dst
   void preorder_copy(const Node* subtree_root_src, Node* subtree_root_dst);
-	
+
   // helper function to remove a node recursively
   Node* remove(const K& key, Node* subtree_root);
   
@@ -91,22 +90,313 @@ private:
   int collection_size; 
 
   // tree root
-  Node* root;
-   
+  Node* root; 
 };
 
 
 // TODO: implement the above functions here ...
+template<typename K, typename V>
+BSTCollection<K,V>::BSTCollection() : collection_size(0), root(nullptr){}
 
-// Note that for the remove helper, since it returns a Node pointer,
-// it must be defined as: 
-//
-//   template<typename K, typename V>
-//   typename BSTCollection<K,V>::Node*
-//   BSTCollection<K,V>::remove(const K& key, Node* subtree_root)
-//   {
-//     ...
-//   }
+template<typename K, typename V>
+BSTCollection<K,V>::BSTCollection(const BSTCollection<K,V>& rhs)
+  : collection_size(0), root(nullptr)
+{
+  *this = rhs;
+}
 
+template<typename K, typename V>
+BSTCollection<K,V>& BSTCollection<K,V>::operator=(const BSTCollection<K,V>& rhs)
+{
+ if (this == &rhs) 
+ {
+  return *this;
+ }
+ // delete current
+ make_empty(root);
+ // build tree
+ std::vector<K> keys;
+ //preorder_copy(root, rhs.root);
+ int val = 0;
+ for(int i = 0; i < rhs.collection_size; i++)
+ {
+  rhs.find(keys[i], val);
+  add(keys[i], val);
+ }
+ return *this;
+}
+
+template<typename K, typename V>
+BSTCollection<K,V>::~BSTCollection()
+{
+  make_empty(root);
+}
+
+// add a new key-value pair into the collection
+template<typename K, typename V>
+void BSTCollection<K,V>::add(const K& a_key, const V& a_val)
+{
+  //create node and set children to nullptr
+  Node* tree = new Node;
+  tree->key = a_key;
+  tree->value = a_val;
+  tree->left = nullptr;
+  tree->right = nullptr;
+
+  //if no root
+  if(root == nullptr)
+  {
+   root = tree;
+   tree->left = nullptr;
+   tree->right = nullptr;
+  }
+  else
+  {
+   Node* cur = new Node;
+   cur = root;
+   while (cur != nullptr)
+   {
+    if(tree->key < cur->key)
+    {
+        if(cur->left == nullptr)
+	{
+         cur->left = tree;
+         cur = nullptr;
+        }
+	else
+	{
+         cur = cur->left;
+        }
+    }
+    else
+    {
+     if(cur->right == nullptr)
+     {
+      cur->right = tree;
+      cur = nullptr;
+     }
+     else
+     {
+      cur = cur->right;
+      tree->left = nullptr;
+      tree->right = nullptr;
+     }
+    }
+   }
+  }
+ //add to size of tree
+ collection_size++;
+}
+
+// remove a key-value pair from the collection
+template<typename K, typename V>
+void BSTCollection<K,V>::remove(const K& a_key)
+{
+  root = remove(a_key, root);
+}
+
+// helper function to remove a node recursively
+template<typename K, typename V>
+typename BSTCollection<K,V>::Node* BSTCollection<K,V>::
+remove(const K& a_key, Node* subtree_root)
+{
+ //if no subtree
+ if(subtree_root == nullptr)
+ {
+  return subtree_root;
+ } 
+ else if(subtree_root && a_key < subtree_root->key)
+ {
+  subtree_root->left = remove(a_key, subtree_root->left);
+ }
+ else if(subtree_root && a_key > subtree_root->key)
+ {
+  subtree_root->right = remove(a_key, subtree_root->right);
+ } 
+ else if(subtree_root && a_key == subtree_root->key)
+ {
+  Node* tree = subtree_root;
+  //if no children
+  if(subtree_root->left == nullptr && subtree_root->right == nullptr)
+  {
+   delete tree;
+   subtree_root = nullptr;
+   collection_size--;
+  } 
+  else if(subtree_root->left != nullptr && subtree_root->right == nullptr)
+  {
+   subtree_root = subtree_root->left;
+   delete tree;
+   tree = nullptr;
+   collection_size--;
+  } 
+  else if(subtree_root->right != nullptr && subtree_root->left == nullptr)
+  {
+   subtree_root = subtree_root->right;
+   delete tree;
+   tree = nullptr;
+   collection_size--;
+  } 
+  else
+  {
+   Node* prev = subtree_root;
+   tree = subtree_root->right;
+   while(tree->left != nullptr)
+   {
+    prev = tree;
+    tree = tree->left;
+   }
+   if(prev != subtree_root)
+   {
+    prev->left = tree->right;
+   }
+   tree->left = subtree_root->left;
+   if(subtree_root->right != tree)
+   {
+    tree->right = subtree_root->right;
+   }
+   //remove and reset values
+   delete subtree_root;
+   subtree_root = tree;
+   prev = nullptr;
+   tree = nullptr;
+   collection_size--;
+  }
+ }
+ return subtree_root;
+}
+
+// find and return the value associated with the key
+template<typename K, typename V>
+bool BSTCollection<K,V>::find(const K& search_key, V& the_val) const
+{
+ Node* tree = root;
+ while(tree != nullptr)
+ {
+  //if correct node return value otherwise search left or right
+  if(tree->key == search_key)
+  {
+   the_val = tree->value;
+   return true;
+  }
+  else if(search_key < tree->key)
+  {
+   tree = tree->left;
+  } 
+  else
+  {
+   tree = tree->right;
+  }
+ }
+ return false;
+}
+
+// find and return the values with keys >= to k1 and <= to k2
+template<typename K, typename V>
+void BSTCollection<K,V>::find(const K& k1, const K& k2,std::vector<V>& vals) const
+{
+ range_search(root, k1, k2, vals);
+}
+
+// helper to recursively find range of keys
+template<typename K, typename V>
+void BSTCollection<K,V>::range_search(const Node* subtree, const K& k1, const K& k2, std::vector<V>& vals)
+const 
+{
+ // use as recursive helper function
+ if(subtree == nullptr)
+  return;
+  
+ range_search(subtree->left, k1, k2, vals);
+
+ if(k1 <= subtree->key && k2 >= subtree->key)
+  vals.push_back(subtree->value);
+
+ range_search(subtree->right, k1, k2, vals);
+}
+
+// return all of the keys in the collection
+template<typename K, typename V>
+void BSTCollection<K,V>::keys(std::vector<K>& all_keys) const
+{
+ inorder(root, all_keys);
+}
+
+// return all of the keys in ascending (sorted) order
+template<typename K, typename V>
+void BSTCollection<K,V>::sort(std::vector<K>& all_keys_sorted) const
+{
+ inorder(root, all_keys_sorted);
+}
+
+// return the number of key-value pairs in the collection
+template<typename K, typename V>
+int BSTCollection<K,V>::size() const
+{
+ return collection_size;
+}
+
+// helper to calc tree height (can use std::max)
+template<typename K, typename V>
+int BSTCollection<K,V>::height(const Node* subtree_root) const
+{
+ if(subtree_root == nullptr)
+  return 0;
+
+ int left = height(subtree_root->left);
+ int right = height(subtree_root->right);
+
+ if(left > right)
+  return(left+1);
+ else
+  return(right+1);
+}
+
+// helper to empty entire tree
+template<typename K, typename V>
+void BSTCollection<K,V>::make_empty(Node* subtree_root) 
+{
+  if(subtree_root == nullptr)
+    return;
+
+  make_empty(subtree_root->left);
+  make_empty(subtree_root->right);
+  subtree_root == nullptr;
+}
+
+// helper to build sorted list of keys (used by keys and sort)
+template<typename K, typename V>
+void BSTCollection<K,V>::inorder(const Node* subtree_root, std::vector<K>& keys) const
+{
+  // recursive helper function
+  if(subtree_root == nullptr)
+    return;
+
+  inorder(subtree_root->left, keys);
+  keys.push_back(subtree_root->key);
+  inorder(subtree_root->right, keys);
+}
+
+// recursively (deep) copy ancestors of src to dst
+template<typename K, typename V>
+void BSTCollection<K,V>::preorder_copy(const Node* subtree_root_src, Node* subtree_root_dst)
+{
+ if (subtree_root_src == nullptr)
+  subtree_root_dst = nullptr;
+ else
+ { 
+  subtree_root_dst = subtree_root_src;
+
+  preorder_copy(subtree_root_src->left, subtree_root_dst->left);
+  preorder_copy(subtree_root_src->right, subtree_root_dst->right);
+ } 
+}
+
+// return the height of the tree
+template<typename K, typename V>
+int BSTCollection<K,V>::height() const
+{
+  return height(root);
+}
 
 #endif
--- hw9_test.cpp
+++ hw9_test.cpp
@@ -1,10 +1,10 @@
 //----------------------------------------------------------------------
-// Author:
+// Author: Kevin Lunden
 // Course: CPSC 223, Spring 2020
 // Assign: 9
 // File:   hw9_test.cpp
 //
-// TODO:   add your own description
+// TODO:   tests the functions of BTSCollection.h
 //----------------------------------------------------------------------
 
 
@@ -105,6 +105,88 @@ TEST(BasicListTest, SimpleSort) {
 
 
 // TODO: ... additional tests ...
+
+// test 6
+// tests sorting with bigger and negative numbers
+TEST(BasicCollectionTest, DifficultSort) {
+  BSTCollection<string,double> c;
+  c.add("a", -10.0);
+  c.add("b", 20.0);
+  c.add("c", 30.0);
+  c.add("d", 40.0);
+  c.add("e", 500.0);
+  vector<string> sorted_keys;
+  c.sort(sorted_keys);
+  ASSERT_EQ(c.size(), sorted_keys.size());
+  for (int i = 0; i < int(sorted_keys.size()) - 1; ++i) {
+    ASSERT_LE(sorted_keys[i], sorted_keys[i+1]);
+  }
+}
+
+//test 7
+//checks removing nonexistent node doesn't impact tree
+//checks removing parent and leaf nodes
+TEST(BasicCollectionTest, DifficultRemoveElems){
+  BSTCollection<char, int> c;
+  ASSERT_EQ(c.height(), 0);
+  ASSERT_EQ(c.size(), 0);
+  c.add('d', 40);
+  c.add('b', 20);
+  c.add('e', 50);
+  c.add('a', 10);
+  c.add('c', 30);
+  c.remove('z');
+  ASSERT_EQ(c.height(), 3);
+  ASSERT_EQ(c.size(), 5);
+  double v;
+  c.remove('b');          
+  ASSERT_EQ(c.height(), 3);
+  ASSERT_EQ(c.size(), 4); 
+  c.remove('e');        
+  ASSERT_EQ(c.height(), 3);
+  ASSERT_EQ(c.size(), 3); 
+}
+
+// Test 8
+// checks entire range working with larger and negative numbers
+TEST(BasicListTest, DifficultRange) {
+  BSTCollection<int,string> c;
+  c.add(50, "e");
+  c.add(-10, "a");
+  c.add(30, "c");
+  c.add(40, "d");
+  c.add(600, "f");
+  c.add(20, "b");
+  vector<string> vs;
+  c.find(-10, 600, vs);
+  ASSERT_EQ(6, vs.size());
+  // note that the following "find" is a C++ built-in function
+  ASSERT_NE(vs.end(), find(vs.begin(), vs.end(), "a"));
+  ASSERT_NE(vs.end(), find(vs.begin(), vs.end(), "b"));
+  ASSERT_NE(vs.end(), find(vs.begin(), vs.end(), "c"));
+  ASSERT_NE(vs.end(), find(vs.begin(), vs.end(), "d"));
+  ASSERT_NE(vs.end(), find(vs.begin(), vs.end(), "e"));
+  ASSERT_NE(vs.end(), find(vs.begin(), vs.end(), "f"));
+}
+
+// Test 9
+// tests added and finding large and negative numbers
+TEST(BasicListTest, DiificultFind) {
+  BSTCollection<string,double> c;
+  double v;
+  ASSERT_EQ(false, c.find("b", v));
+  c.add("b", 100.0);
+  ASSERT_EQ(true, c.find("b", v));
+  ASSERT_EQ(100.0, v);
+  ASSERT_EQ(false, c.find("a", v));
+  c.add("a", -20.0);
+  ASSERT_EQ(true, c.find("a", v));
+  ASSERT_EQ(-20.0, v);
+  ASSERT_EQ(false, c.find("c", v));
+  c.add("c", 10000.0);
+  ASSERT_EQ(true, c.find("c", v));
+  ASSERT_EQ(10000.0, v);
+}
 
 int main(int argc, char** argv)
 {
